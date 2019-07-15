25f156053fccd7b082ada06978fd106fab115345
jonnyeom <jonnyeom@gmail.com>
2019-07-15 14:49:54 -0400

    Fix Broken Plugin to correctly initialize

--- src/Plugin/SalesforceAuthProvider/Broken.php
+++ src/Plugin/SalesforceAuthProvider/Broken.php
@@ -5,6 +5,9 @@ namespace Drupal\salesforce\Plugin\SalesforceAuthProvider;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\salesforce\Consumer\SalesforceCredentials;
 use Drupal\salesforce\SalesforceAuthProviderPluginBase;
+use Drupal\salesforce\Storage\SalesforceAuthTokenStorageInterface;
+use OAuth\Common\Http\Client\ClientInterface;
+use OAuth\Common\Http\Uri\Uri;
 use OAuth\Common\Token\TokenInterface;
 use OAuth\OAuth2\Service\Exception\MissingRefreshTokenException;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -29,10 +32,16 @@ class Broken extends SalesforceAuthProviderPluginBase {
    *   Plugin id.
    * @param mixed $plugin_definition
    *   Plugin definition.
+   * @param \OAuth\Common\Http\Client\ClientInterface $httpClient
+   *   The oauth http client.
+   * @param \Drupal\salesforce\Storage\SalesforceAuthTokenStorageInterface $storage
+   *   Auth token storage service.
+   *
+   * @throws \OAuth\OAuth2\Service\Exception\InvalidScopeException
+   *   Comment.
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition) {
-    $this->configuration = $configuration;
-    $this->pluginDefinition = $plugin_definition;
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ClientInterface $httpClient, SalesforceAuthTokenStorageInterface $storage) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $httpClient, $storage);
     $this->id = $plugin_id;
     $this->credentials = new SalesforceCredentials('', '', '');
   }
@@ -48,7 +57,7 @@ class Broken extends SalesforceAuthProviderPluginBase {
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static($configuration, $plugin_id, $plugin_definition);
+    return new static($configuration, $plugin_id, $plugin_definition, $container->get('salesforce.http_client_wrapper'), $container->get('salesforce.auth_token_storage'));
   }
 
   /**
